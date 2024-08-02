c1adcf839e8ae8b9ccb048c555b859e19443ef01
Laturas <laturaas@gmail.com>
2024-08-02 12:19:32 -0400

    Power cube true ending done

--- My project (2)/Assets/scripts/RandomBS/ThugShaker.cs
+++ My project (2)/Assets/scripts/RandomBS/ThugShaker.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 public class ThugShaker : MonoBehaviour
 {
     private Vector3 originalPosition;
+    [SerializeField] private Vector3 originalPositionOverride;
+    [SerializeField] private bool overridePos = false;
     public float shakeStrength;
     void Awake() => originalPosition = Vector3.zero;
+    
     // Update is called once per frame
     void Update()
     {
         if (originalPosition == Vector3.zero && shakeStrength != 0) {
-            originalPosition = transform.position;
+            originalPosition = transform.localPosition;
         }
-        transform.position = originalPosition + (Random.onUnitSphere * shakeStrength);
+        if (overridePos) {
+            originalPosition = originalPositionOverride;
+        }
+        transform.localPosition = originalPosition + (Random.onUnitSphere * shakeStrength);
     }
 }
---
+++ My project (2)/Assets/scripts/TrueEnding.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrueEnding : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
