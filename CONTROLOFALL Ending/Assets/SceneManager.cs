f51f3ee08e9bc91960960845d272f9d04604c0a8
Laturas <laturaas@gmail.com>
2024-08-07 22:53:15 -0400

    Tree scene, FINDME puzzle made

--- CONTROLOFALL Ending/Assets/SceneManager.cs
+++ CONTROLOFALL Ending/Assets/SceneManager.cs
@@ -23,11 +23,18 @@ public class SceneManager : MonoBehaviour
             case 3:
             case 4:
             case 5:
-                goto case 7;
+                goto case 12;
             case 6:
                 postProc.SetActive(false);
-                goto case 7;
+                goto case 12;
             case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+                postProc.SetActive(true);
+                goto case 12;
+            case 12:
             default:
                 scenes[phase].SetActive(false);
                 phase++;
