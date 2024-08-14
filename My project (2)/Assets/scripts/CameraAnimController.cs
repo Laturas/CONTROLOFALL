6c4beb630a17e0acc083ed0b798e8ecd21916f2a
Laturas <laturaas@gmail.com>
2024-08-14 17:06:03 -0400

    What the fuck did I finish powercube??????

--- My project (2)/Assets/scripts/CameraAnimController.cs
+++ My project (2)/Assets/scripts/CameraAnimController.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class CameraAnimController : MonoBehaviour
 {
+    private AudioListener theAud;
+    void Awake() => theAud = GetComponent<AudioListener>();
     // Start is called before the first frame update
     void Start()
     {
         PlayerProperties.canMove = true;
+    }
+    public void killAudio() {
+        theAud.enabled = false;
     }
 }
--- My project (2)/Assets/scripts/FakeEnd.cs
+++ My project (2)/Assets/scripts/FakeEnd.cs
@@ -6,8 +6,11 @@ public class FakeEnd : MonoBehaviour
 {
     public GameObject playerCam;
     public GameObject HOLP;
+    public GameObject neverDieParticles;
     private Animator playerCamAnim;
     public Transform thingToLookAt;
+    public Transform playerTransform;
+    public Transform camHolp;
     private AudioSource thisAud;
     private Animator thisAnim;
     public float[] timers;
@@ -25,8 +28,10 @@ public class FakeEnd : MonoBehaviour
         PlayerProperties.canMove = false;
         playerCamAnim.Play("InterestingCutscene");
     }
-    void Update() {
+    void LateUpdate() {
         if (!started) {return;}
+        if (cays == 5) {neverDieParticles.SetActive(false); cays++; return;}
+        if (cays == 6) {return;}
 
         timers[cays] -= Time.deltaTime;
         if (timers[cays] <= 0f) {
@@ -34,13 +39,38 @@ public class FakeEnd : MonoBehaviour
             if (cays == 1) {
                 thisAnim.Play("IdkWhatToNameThis");
             }
+            if (cays == 3) {
+                playerCamAnim.Play("CutscenePt2");
+            }
+            if (cays == 4) {
+                playerCamAnim.Play("uowthgergthtrw");
+                neverDieParticles.SetActive(true);
+            }
         }
 
         switch (cays) {
-            case 0:
+            case 0: {
                 playerCam.transform.position = Vector3.Lerp(playerCam.transform.position, HOLP.transform.position, Time.deltaTime);
-                var lerpRot = Quaternion.Lerp(playerCam.transform.rotation, Quaternion.LookRotation(thingToLookAt.position - playerCam.transform.position, thingToLookAt.up), Time.deltaTime);
-                playerCam.transform.rotation = lerpRot;
+                playerCam.transform.rotation = 
+                Quaternion.Lerp(playerCam.transform.rotation, Quaternion.LookRotation(thingToLookAt.position - playerCam.transform.position, thingToLookAt.up), Time.deltaTime);
+                return;
+            }
+            case 1: return;
+            case 2: {
+                playerCam.transform.position = Vector3.Lerp(playerCam.transform.position, camHolp.position, Time.deltaTime);
+                playerCam.transform.rotation = 
+                Quaternion.Lerp(playerCam.transform.rotation, Quaternion.LookRotation(playerTransform.position - playerCam.transform.position, playerTransform.up), Time.deltaTime);
+                return;
+            }
+            case 3:
+                playerCam.transform.position = Vector3.Lerp(playerCam.transform.position, HOLP.transform.position, Time.deltaTime);
+                playerCam.transform.rotation = 
+                Quaternion.Lerp(playerCam.transform.rotation, Quaternion.LookRotation(thingToLookAt.position - playerCam.transform.position, thingToLookAt.up), Time.deltaTime);
+                return;
+            case 4:
+                playerCam.transform.position = Vector3.Lerp(playerCam.transform.position, HOLP.transform.position, Time.deltaTime);
+                playerCam.transform.rotation = 
+                Quaternion.Lerp(playerCam.transform.rotation, Quaternion.LookRotation(thingToLookAt.position - playerCam.transform.position, thingToLookAt.up), Time.deltaTime);
                 return;
             default:
                 return;
